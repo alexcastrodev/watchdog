afa60cf57f1771876e0a3d3370418a849f542d9e
alexoliveira7x@gmail.com <alexoliveira7x@gmail.com>
2025-09-30 00:54:33 +0100

    replace listen to redis pub sub

--- server.rb
+++ server.rb
@@ -157,22 +157,26 @@ get '/api/job/:filename/tail' do
     end
 end
 
-post '/api/webhook/build' do
-    request.body.rewind
-    payload = JSON.parse(request.body.read) rescue {}
-    
-    halt 422, {'Content-Type' => 'application/json'}, { error: 'missing stack' }.to_json if !payload['stack']
+require 'redis'
 
-    jobs_dir = File.join(__dir__, 'jobs')
-    Dir.mkdir(jobs_dir) unless Dir.exist?(jobs_dir)
+Thread.new do
+  redis = Redis.new(host: "127.0.0.1", port: 9001)
 
-    stack_name = payload['stack']
-    file_path = File.join(jobs_dir, "#{stack_name}")
+  redis.subscribe('watchdog') do |on|
+    on.message do |channel, msg|
+      # msg => {"stack":"watchdog-webhook"}
+      payload = JSON.parse(msg)
+      jobs_dir = File.join(__dir__, 'jobs')
+      Dir.mkdir(jobs_dir) unless Dir.exist?(jobs_dir)
 
-    @job = Job.new(name: stack_name).create_job
+      stack_name = payload['stack']
+      file_path = File.join(jobs_dir, "#{stack_name}")
 
-    status 201
-    { message: "Job file created for stack #{stack_name}" }.to_json
+      @job = Job.new(name: stack_name).create_job
+      
+      executor_path = File.join(File.dirname(__FILE__), 'executor.rb')
+      job_file_path = File.join(File.dirname(__FILE__), 'jobs', "#{payload["stack"]}.yml")
+      spawn("ruby", executor_path, job_file_path)
+    end
+  end
 end
-
-require_relative 'scheduler'
